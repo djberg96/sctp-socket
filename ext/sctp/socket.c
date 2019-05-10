bf7b3317ac4aa34fdfe26a67c29ca0f9a90c56f2
Daniel Berger <dberger@redhat.com>
2019-05-10 15:17:20 -0400

    Move out variables into their own files.

--- ext/sctp/socket.c
+++ ext/sctp/socket.c
@@ -1,11 +1,10 @@
 #include "ruby.h"
 #include <netinet/sctp.h>
 
-void Init_socket(){
-  VALUE mSCTP, cSocket, cClient, cServer;
+VALUE mSCTP;
+VALUE cSocket;
 
+void Init_socket(){
   mSCTP   = rb_define_module("SCTP");
   cSocket = rb_define_class_under(mSCTP, "Socket", rb_cObject);
-  cClient = rb_define_class_under(cSocket, "Client", rb_cObject);
-  cServer = rb_define_class_under(cSocket, "Server", rb_cObject);
 }
