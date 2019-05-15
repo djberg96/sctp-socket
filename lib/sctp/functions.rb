d971b6980f916f182adc32adb314132575eb3c20
Daniel Berger <djberg96@gmail.com>
2019-05-15 07:49:52 -0400

    Remove apparently unsupported functions, reorganize current functions alphabetically.

--- lib/sctp/functions.rb
+++ lib/sctp/functions.rb
@@ -6,17 +6,15 @@ module SCTP
     ffi_lib :sctp
 
     attach_function :sctp_bindx, [:int, :pointer, :int, :int], :int
-    attach_function :sctp_sendmsg, [:int, :pointer, :size_t, :pointer, :pointer, :uint32, :uint32, :uint16, :uint32, :uint32], :int
-    #attach_function :sctp_sendv, [:int, :pointer, :int, :pointer, :int, :pointer, :pointer, :uint, :int], :int
-    attach_function :sctp_send, [:int, :pointer, :size_t, :pointer, :uint32], :int
-    attach_function :sctp_recvmsg, [:int, :pointer, :size_t, :pointer, :pointer, :pointer, :pointer], :int
-    attach_function :sctp_peeloff, [:int, :uint32], :int
-    #attach_function :sctp_peeloff_flags, [:int, :uint32, :uint], :int
-    attach_function :sctp_getpaddrs, [:int, :uint32, :pointer], :int
+    attach_function :sctp_connectx, [:int, :pointer, :int, :uint32], :int
+    attach_function :sctp_freeladdrs, [:pointer], :void
     attach_function :sctp_freepaddrs, [:pointer], :void
     attach_function :sctp_getladdrs, [:int, :uint32, :pointer], :int
-    attach_function :sctp_freeladdrs, [:pointer], :void
+    attach_function :sctp_getpaddrs, [:int, :uint32, :pointer], :int
     attach_function :sctp_opt_info, [:int, :uint32, :int, :pointer, :pointer], :int
-    attach_function :sctp_connectx, [:int, :pointer, :int, :uint32], :int
+    attach_function :sctp_peeloff, [:int, :uint32], :int
+    attach_function :sctp_recvmsg, [:int, :pointer, :size_t, :pointer, :pointer, :pointer, :pointer], :int
+    attach_function :sctp_send, [:int, :pointer, :size_t, :pointer, :uint32], :int
+    attach_function :sctp_sendmsg, [:int, :pointer, :size_t, :pointer, :pointer, :uint32, :uint32, :uint16, :uint32, :uint32], :int
   end
 end
