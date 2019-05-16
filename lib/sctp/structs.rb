a41275298a047a0efe1ce325e84f9450165ecb5b
Daniel Berger <djberg96@gmail.com>
2019-05-16 19:02:55 -0400

    Added SockAddrIn struct.

--- lib/sctp/structs.rb
+++ lib/sctp/structs.rb
@@ -10,5 +10,17 @@ module SCTP
         :sa_data, [:char, 14]
       )
     end
+
+    class InAddr < FFI::Struct
+      layout(:s_addr, :uint32)
+    end
+
+    class SockAddrIn < FFI::Struct
+      layout(
+        :sin_family, :sa_family_t,
+        :sin_port, :in_port_t,
+        :sin_addr, InAddr
+      )
+    end
   end
 end
