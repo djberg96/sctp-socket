cea416d48a15cc24d4890ebad9ff6583309bfa48
Daniel Berger <djberg96@gmail.com>
2019-05-16 19:13:00 -0400

    Added the SctpSndrcvinfo struct.

--- lib/sctp/structs.rb
+++ lib/sctp/structs.rb
@@ -4,6 +4,8 @@ module SCTP
   module Structs
     extend FFI::Library
 
+    typedef :int32, :sctp_assoc_t
+
     class Sockaddr < FFI::Struct
       layout(
         :sa_family, :ushort,
@@ -20,6 +22,20 @@ module SCTP
         :sin_family, :sa_family_t,
         :sin_port, :in_port_t,
         :sin_addr, InAddr
+      )
+    end
+
+    class SctpSndrcvinfo < FFI::Struct
+      layout(
+	      :sinfo_stream, :uint16,
+	      :sinfo_ssn, :uint16,
+	      :sinfo_flags, :uint16,
+	      :sinfo_ppid, :uint32,
+	      :sinfo_context, :uint32,
+	      :sinfo_timetolive, :uint32,
+	      :sinfo_tsn, :uint32,
+	      :sinfo_cumtsn, :uint32,
+	      :sinfo_assoc_id, :sctp_assoc_t
       )
     end
   end
