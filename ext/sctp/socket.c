4f06c9bb7614b27a7b5b0aeb838b418fb8d62197
Daniel Berger <djberg96@gmail.com>
2020-11-16 15:55:57 -0500

    Add initialize and close functions.

--- ext/sctp/socket.c
+++ ext/sctp/socket.c
@@ -1,10 +1,48 @@
 #include "ruby.h"
+#include <string.h>
+#include <errno.h>
 #include <netinet/sctp.h>
 
 VALUE mSCTP;
 VALUE cSocket;
 
+static VALUE sctp_init(int argc, VALUE* argv, VALUE self){
+  int sock_fd;
+  VALUE v_domain, v_type;
+
+  rb_scan_args(argc, argv, "02", &v_domain, &v_type);
+
+  if(NIL_P(v_domain))
+    v_domain = INT2NUM(AF_INET);
+  
+  if(NIL_P(v_type))
+    v_type = INT2NUM(SOCK_SEQPACKET);
+
+  sock_fd = socket(NUM2INT(v_domain), NUM2INT(v_type), IPPROTO_SCTP);
+
+  if(sock_fd < 0)
+    rb_raise(rb_eSystemCallError, "socket: %s", strerror(errno));
+
+  rb_iv_set(self, "@domain", v_domain);
+  rb_iv_set(self, "@type", v_type);
+  rb_iv_set(self, "@sock_fd", INT2NUM(sock_fd));
+
+  return self;
+}
+
+static VALUE sctp_close(VALUE self){
+  VALUE v_sock_fd = rb_iv_get(self, "@sock_fd");
+
+  if(close(NUM2INT(v_sock_fd)))
+    rb_raise(rb_eSystemCallError, "close: %s", strerror(errno));
+
+  return self;
+}
+
 void Init_socket(){
   mSCTP   = rb_define_module("SCTP");
   cSocket = rb_define_class_under(mSCTP, "Socket", rb_cObject);
+
+  rb_define_method(cSocket, "initialize", sctp_init, -1);
+  rb_define_method(cSocket, "close", sctp_close, 0);
 }
