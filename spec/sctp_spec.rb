7589874a061be60281eff13db3a7c0b3dc900d52
Daniel Berger <djberg96@gmail.com>
2020-11-17 09:21:01 -0500

    Added some specs.

--- spec/sctp_spec.rb
+++ spec/sctp_spec.rb
@@ -2,11 +2,21 @@ require 'rspec'
 require 'sctp/socket'
 require 'mkmf/lite'
 
-RSpec.describe SCTP::Socket do
+RSpec.describe SCTPSocket do
   context "structs" do
     include Mkmf::Lite
+
+    example "SCTP::Struct::Sockaddr is the expected size" do
+      expect(check_sizeof("struct sockaddr", "arpa/inet.h")).to eq(SCTP::Structs::Sockaddr.size)
+    end
+
     example "SCTP::Struct::SockAddrIn is the expected size" do
       expect(check_sizeof("struct sockaddr_in", "arpa/inet.h")).to eq(SCTP::Structs::SockAddrIn.size)
     end
+
+    example "SCTP::Struct::InAddr is the expected size" do
+      expect(check_sizeof("struct in_addr", "arpa/inet.h")).to eq(SCTP::Structs::InAddr.size)
+    end
+
   end
 end
