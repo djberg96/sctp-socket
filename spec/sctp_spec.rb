769d9eccc118600ba542597c3cabbaa9232e5676
Daniel Berger <djberg96@gmail.com>
2020-11-29 13:44:41 -0500

    Added a version spec.

--- spec/sctp_spec.rb
+++ spec/sctp_spec.rb
@@ -1,6 +1,12 @@
 require 'sctp/socket'
 
 RSpec.describe SCTP::Socket do
+  context "version" do
+    example "version is set to the expected value" do
+      expect(SCTP::Socket::VERSION).to eq('0.0.2')
+    end
+  end
+
   context "basic" do
     example "basic" do
       expect(described_class).to be(described_class)
