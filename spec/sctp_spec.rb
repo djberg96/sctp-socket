241092be4c11de089d2deb3366cbbb1cd88090d2
Daniel Berger <djberg96@gmail.com>
2020-12-06 12:22:33 -0500

    Add more specs.

--- spec/sctp_spec.rb
+++ spec/sctp_spec.rb
@@ -3,9 +3,11 @@ require 'sctp/socket'
 require 'mkmf/lite'
 
 RSpec.describe SCTPSocket do
-  context "structs" do
-    include Mkmf::Lite
+  include Mkmf::Lite
 
+  let(:sctp_header){ 'netinet/sctp.h' }
+
+  context "structs" do
     example "SCTP::Struct::Sockaddr is the expected size" do
       expect(check_sizeof("struct sockaddr", "arpa/inet.h")).to eq(SCTP::Structs::Sockaddr.size)
     end
@@ -17,6 +19,25 @@ RSpec.describe SCTPSocket do
     example "SCTP::Struct::InAddr is the expected size" do
       expect(check_sizeof("struct in_addr", "arpa/inet.h")).to eq(SCTP::Structs::InAddr.size)
     end
+  end
 
+  context "C functions" do
+    include SCTP::Functions
+
+    example "htons returns the expected value" do
+      expect(c_htons(42000)).to eq(4260)
+    end
+
+    example "inet_addr returns the expected value" do
+      expect(c_inet_addr('127.0.0.1')).to eq(16777343)
+    end
+  end
+
+  context "constants" do
+    include SCTP::Constants
+
+    example "SCTP_BINDX_ADD_ADDR is set to the expected value" do
+      expect(check_valueof("SCTP_BINDX_ADD_ADDR", sctp_header)).to eq(SCTP::Constants::SCTP_BINDX_ADD_ADDR)
+    end
   end
 end
