1a8832a77162d77cc97db7e698c2a0197d43340f
Daniel Berger <djberg96@gmail.com>
2021-08-19 23:43:06 -0400

    Add check for SCTP_EMPTY.

--- ext/sctp/extconf.rb
+++ ext/sctp/extconf.rb
@@ -3,7 +3,10 @@ require 'mkmf'
 dir_config('sctp')
 
 have_header('netinet/sctp.h')
-have_header('usrsctp.h')
+
+if have_header('usrsctp.h')
+  have_const('SCTP_EMPTY', 'usrsctp.h')
+end
 
 have_library('sctp')
 have_library('usrsctp')
