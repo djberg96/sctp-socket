44722dd3228fe26b0a6dc4a308337af6bd42cad8
Daniel Berger <djberg96@gmail.com>
2021-12-15 13:40:23 -0500

    Bump version to 0.0.5.

--- spec/sctp_spec.rb
+++ spec/sctp_spec.rb
@@ -3,7 +3,7 @@ require 'sctp/socket'
 RSpec.describe SCTP::Socket do
   context "version" do
     example "version is set to the expected value" do
-      expect(SCTP::Socket::VERSION).to eq('0.0.4')
+      expect(SCTP::Socket::VERSION).to eq('0.0.5')
     end
   end
 
