b3c903a436dd2969e9353b23dabd06e049e302c8
Daniel Berger <djberg96@gmail.com>
2022-11-23 06:10:29 -0500

    Fix usrsctp_bind declaration and usrsctp_init, add bindx, connectx.

--- lib/sctp/darwin/functions.rb
+++ lib/sctp/darwin/functions.rb
@@ -10,14 +10,20 @@ module SCTP
 
     callback :receive_cb, [:pointer, SctpSockstore, :pointer, :size_t, SctpRcvinfo, :int], :int
     callback :send_cb, %i[pointer uint32_t], :int
+    callback :sctp_outbound_packet, %i[pointer pointer size_t uint8_t uint8_t], :int
+
+    # Callbacks can't have varargs, using a pointer for now
+    callback :sctp_debug_printf, %i[string pointer], :void
 
     attach_function :usrsctp_accept, %i[pointer pointer pointer], :pointer
-    attach_function :usrsctp_bind, %i[pointer pointer pointer], :int
+    attach_function :usrsctp_bind, %i[pointer pointer socklen_t], :int
+    attach_function :usrsctp_bindx, %i[pointer pointer int int], :int
     attach_function :usrsctp_close, %i[pointer], :void
     attach_function :usrsctp_connect, %i[pointer pointer pointer], :int
+    attach_function :usrsctp_connectx, %i[pointer pointer int pointer], :int
     attach_function :usrsctp_finish, [], :int
     attach_function :usrsctp_getsockopt, %i[pointer int int pointer pointer], :int
-    attach_function :usrsctp_init, %i[uint16], :void
+    attach_function :usrsctp_init, %i[uint16 sctp_outbound_packet sctp_debug_printf], :void
     attach_function :usrsctp_listen, %i[pointer int], :int
     attach_function :usrsctp_recvv, %i[pointer pointer size_t pointer pointer pointer pointer pointer], :ssize_t
     attach_function :usrsctp_socket, %i[int int int receive_cb send_cb uint32_t pointer], :pointer
