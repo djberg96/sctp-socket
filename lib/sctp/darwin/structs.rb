b0dce43ee5e36394063736e30a41443532866971
Daniel Berger <djberg96@gmail.com>
2022-11-23 06:10:57 -0500

    Add Sockaddr, InAddr and SockAddrIn.

--- lib/sctp/darwin/structs.rb
+++ lib/sctp/darwin/structs.rb
@@ -6,6 +6,27 @@ module SCTP
 
     typedef :uint32_t, :sctp_assoc_t
 
+    class Sockaddr < FFI::Struct
+      layout(
+        :sa_family, :ushort,
+        :sa_data, [:char, 14]
+      )
+    end
+
+    class InAddr < FFI::Struct
+      layout(:s_addr, :uint32)
+    end
+
+    class SockAddrIn < FFI::Struct
+      layout(
+        :sin_len, :uint8_t,
+        :sin_family, :sa_family_t,
+        :sin_port, :in_port_t,
+        :sin_addr, InAddr,
+        :sin_zero, [:char, 8]
+      )
+    end
+
     class SockaddrStorage < FFI::Struct
       layout(
         :ss_family, :sa_family_t,
