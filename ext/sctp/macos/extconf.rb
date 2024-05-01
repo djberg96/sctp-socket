cfe3b6ee848595ee89204cf0c129ee63197a89b2
Daniel Berger <daniel.berger@covermymeds.com>
2024-05-01 06:27:07 -0400

    Search homebrew libs.

--- ext/sctp/macos/extconf.rb
+++ ext/sctp/macos/extconf.rb
@@ -2,8 +2,8 @@ require 'mkmf'
 
 dir_config('sctp')
 
-find_header('usrsctp.h', '/usr/local/include')
-find_library('usrsctp', nil, '/usr/local/lib')
+find_header('usrsctp.h', '/usr/local/include', '/opt/homebrew/include')
+find_library('usrsctp', nil, '/usr/local/lib', '/opt/homebrew/lib')
 
 if have_header('usrsctp.h')
   have_const('SCTP_EMPTY', 'usrsctp.h')
