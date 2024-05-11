35f5f3771bb738b8cb10080a4e6728d31993f43a
Daniel Berger <78529+djberg96@users.noreply.github.com>
2024-05-11 17:54:43 -0400

    Use a loop to be more interesting.

--- examples/client_example.rb
+++ examples/client_example.rb
@@ -8,8 +8,20 @@ addresses = ['1.1.1.1', '1.1.1.2']
 begin
   port = 62324
   socket = SCTP::Socket.new
-  bytes_sent = socket.sendmsg(:message => "Hello World!", :addresses => addresses, :port => port, :stream => 2)
-  p bytes_sent
+  5.times{ |n|
+    stream = [*1..5].sample
+    puts "Stream: #{stream}"
+    bytes_sent = socket.sendmsg(
+      :message   => "#{n+1} Hello World!",
+      :addresses => addresses.shuffle,
+      :port      => port,
+      :stream    => stream,
+      :ppid      => 1234567,
+      #:flags     => SCTP::Socket::SCTP_UNORDERED | SCTP::Socket::SCTP_SENDALL,
+      :ttl       => 100,
+    )
+    puts "Bytes sent: #{bytes_sent}"
+  }
 ensure
   socket.close if socket
 end
