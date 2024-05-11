b75a2dd9185c8e1dbe17696e52d9a50a2cacf555
Daniel Berger <78529+djberg96@users.noreply.github.com>
2024-05-11 12:52:52 -0400

    Add instructions for creating test interfaces.

--- examples/client_example.rb
+++ examples/client_example.rb
@@ -1,10 +1,14 @@
 require 'socket'
 require 'sctp/socket'
 
+# Adjust as needed. Server server_example.rb for creating
+# fake network interfaces for testing.
+addresses = ['1.1.1.1', '1.1.1.2']
+
 begin
   port = 62324
   socket = SCTP::Socket.new
-  bytes_sent = socket.sendmsg(:message => "Hello World!", :addresses => ['127.0.0.1'], :port => port, :stream => 2)
+  bytes_sent = socket.sendmsg(:message => "Hello World!", :addresses => addresses, :port => port, :stream => 2)
   p bytes_sent
 ensure
   socket.close if socket
--- examples/server_example.rb
+++ examples/server_example.rb
@@ -1,10 +1,26 @@
 require 'sctp/socket'
 
+# To test multiple IP addresses locally:
+#
+# sudo apt install iproute2
+# Add 'dummy' to /etc/modules
+#
+# sudo ip link add dummy1 type dummy
+# sudo ip link add dummy2 type dummy
+#
+# sudo ip addr add 1.1.1.1/24 dev dummy1
+# sudo ip addr add 1.1.1.2/24 dev dummy2
+#
+# sudo ip link set dummy1 up
+# sudo ip link set dummy2 up
+
 # Adjust IP addresses as needed
+addresses = ['1.1.1.1', '1.1.1.2']
+
 begin
   port = 62324
   socket = SCTP::Socket.new
-  socket.bind(:port => port, :addresses => ['10.0.5.5', '10.0.6.5'])
+  socket.bind(:port => port, :addresses => addresses)
   socket.set_initmsg(:output_streams => 5, :input_streams => 5, :max_attempts => 4)
   socket.listen
 
