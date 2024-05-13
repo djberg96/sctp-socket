af94d2c8581790308fb26a0579ece5973a67236b
Daniel Berger <78529+djberg96@users.noreply.github.com>
2024-05-13 00:33:27 -0400

    Fix stream numbers, add bind and connect.

--- examples/client_example.rb
+++ examples/client_example.rb
@@ -8,19 +8,26 @@ addresses = ['1.1.1.1', '1.1.1.2']
 begin
   port = 62324
   socket = SCTP::Socket.new
-  5.times{ |n|
-    stream = [*1..5].sample
+
+  # Optional, but could bind to a subset of available addresses
+  p socket.bind(:addresses => addresses)
+
+  # Initial connection
+  p socket.connect(:addresses => addresses, :port => port)
+
+  3.times{ |n|
+    stream = [*0..4].sample # Max 5 streams in server example
     puts "Stream: #{stream}"
     bytes_sent = socket.sendmsg(
       :message   => "#{n+1} Hello World!",
       :addresses => addresses.shuffle,
-      :port      => port,
+      :port      => port, # Not sure why I have to specify this again, still working out the kinks
       :stream    => stream,
       :ppid      => 1234567,
-      #:flags     => SCTP::Socket::SCTP_UNORDERED | SCTP::Socket::SCTP_SENDALL,
+      # :flags     => SCTP::Socket::SCTP_UNORDERED | SCTP::Socket::SCTP_SENDALL,
       :ttl       => 100,
     )
-    puts "Bytes sent: #{bytes_sent}"
+    puts "#{n+1}: Bytes sent: #{bytes_sent}"
   }
 ensure
   socket.close if socket
