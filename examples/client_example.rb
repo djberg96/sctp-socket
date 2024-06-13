373b0dc941b0ff60ed39d47664a5ca877cf4844c
Daniel Berger <78529+djberg96@users.noreply.github.com>
2024-06-12 23:29:02 -0400

    Bring examples up to date.

--- examples/client_example.rb
+++ examples/client_example.rb
@@ -1,11 +1,41 @@
 require 'socket'
 require 'sctp/socket'
 
+# Adjust as needed. Server server_example.rb for creating
+# fake network interfaces for testing.
+addresses = ['1.1.1.1', '1.1.1.2']
+
 begin
   port = 62324
   socket = SCTP::Socket.new
-  bytes_sent = socket.sendmsg(:message => "Hello World!", :addresses => ['127.0.0.1'], :port => port, :stream => 2)
-  p bytes_sent
+
+  # Optional, but could bind to a subset of available addresses
+  p socket.bindx(:addresses => addresses)
+
+  # Initial connection
+  p socket.connectx(:addresses => addresses, :port => port)
+  p socket.get_status
+
+  # Try a sendv
+  p socket.sendv(:message => ["Hello ", "World!"])
+
+  # Send messages on separate streams of the same connection
+  arr = []
+
+  0.upto(4) do |n|
+    arr << Thread.new do |t|
+      puts "Stream: #{n}"
+      bytes_sent = socket.sendmsg(
+        :message   => "Hello World: #{n+1}",
+        :addresses => addresses.shuffle,
+        :stream    => n,
+        :port      => port
+      )
+      puts "Bytes Sent: #{bytes_sent}"
+    end
+  end
+
+  arr.map(&:join)
 ensure
   socket.close if socket
 end
--- examples/server_example.rb
+++ examples/server_example.rb
@@ -1,11 +1,31 @@
 require 'sctp/socket'
 
+# rake compile + ruby -Ilib to run local version
+puts "VERSION: #{SCTP::Socket::VERSION}"
+
+# To test multiple IP addresses locally:
+#
+# sudo apt install iproute2
+# Add 'dummy' to /etc/modules
+#
+# sudo ip link add dummy1 type dummy
+# sudo ip link add dummy2 type dummy
+#
+# sudo ip addr add 1.1.1.1/24 dev dummy1
+# sudo ip addr add 1.1.1.2/24 dev dummy2
+#
+# sudo ip link set dummy1 up
+# sudo ip link set dummy2 up
+
 # Adjust IP addresses as needed
+addresses = ['1.1.1.1', '1.1.1.2']
+
 begin
   port = 62324
   socket = SCTP::Socket.new
-  socket.bind(:port => port, :addresses => ['10.0.5.5', '10.0.6.5'])
+  socket.bindx(:port => port, :addresses => addresses)
   socket.set_initmsg(:output_streams => 5, :input_streams => 5, :max_attempts => 4)
+  socket.subscribe(:data_io => true, :shutdown => true, :send_failure => true, :partial_delivery => true)
   socket.listen
 
   while true
