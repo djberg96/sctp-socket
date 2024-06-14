d2eaa26e675e54a7cf4ac899bd8f151344230a74
Daniel Berger <78529+djberg96@users.noreply.github.com>
2024-06-13 21:32:28 -0400

    Copy over latest from main.

--- ext/linux/sctp/extconf.rb
+++ ext/linux/sctp/extconf.rb
@@ -1,5 +1,4 @@
 require 'mkmf'
-require 'rbconfig'
 
 dir_config('sctp')
 
@@ -27,5 +26,6 @@ end
 
 have_library('sctp')
 have_func('sctp_sendv', 'netinet/sctp.h')
+have_func('sctp_recvv', 'netinet/sctp.h')
 have_struct_member('struct sctp_event_subscribe', 'sctp_send_failure_event', 'netinet/sctp.h')
 create_makefile('sctp/socket')
