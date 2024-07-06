ccb938753d845931811854a019dab962b0c71a61
Daniel Berger <78529+djberg96@users.noreply.github.com>
2024-07-06 08:13:11 -0400

    Remove instructions (just use wiki) and check for usrsctp.

--- ext/sctp/extconf.rb
+++ ext/sctp/extconf.rb
@@ -2,30 +2,15 @@ require 'mkmf'
 
 dir_config('sctp')
 
-unless have_header('netinet/sctp.h')
-  os = IO.readlines('/etc/os-release').first.split('=').last
-  msg = "\nSCTP HEADERS NOT FOUND. PLEASE INSTALL THEM FIRST LIKE SO:\n\n"
-
-  if os =~ /red|fedora|centos/i
-    msg << "#####################################################################################\n"
-    msg << "# dnf install lksctp-tools                                                          #\n"
-    msg << "# dnf install kernel-modules-extra                                                  #\n"
-    msg << "#                                                                                   #\n"
-    msg << "# sed -e '/blacklist sctp/s/^b/#b/g' -i /etc/modprobe.d/sctp-blacklist.conf         #\n"
-    msg << "# sed -e '/blacklist sctp/s/^b/#b/g' -i /etc/modprobe.d/sctp_diag-blacklist.conf    #\n"
-    msg << "#                                                                                   #\n"
-    msg << "# sudo systemctl restart systemd-modules-load.service                               #\n"
-    msg << "#####################################################################################\n"
-  else
-    msg << "sudo apt-get install libsctp-dev lksctp-tools\n\n"
-  end
-
-  warn msg
-  exit
+# Prefer libusrsctp if found.
+if have_header('usrsctp.h')
+  have_library('usrsctp')
+else
+  have_header('netinet/sctp.h')
+  have_library('sctp')
+  have_func('sctp_sendv', 'netinet/sctp.h')
+  have_func('sctp_recvv', 'netinet/sctp.h')
+  have_struct_member('struct sctp_event_subscribe', 'sctp_send_failure_event', 'netinet/sctp.h')
 end
 
-have_library('sctp')
-have_func('sctp_sendv', 'netinet/sctp.h')
-have_func('sctp_recvv', 'netinet/sctp.h')
-have_struct_member('struct sctp_event_subscribe', 'sctp_send_failure_event', 'netinet/sctp.h')
 create_makefile('sctp/socket')
