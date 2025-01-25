ab696996b6e0718806c7cbe6514221418c778683
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-01-24 22:11:14 -0500

    Add specs for get_association_info.

--- spec/sctp_spec.rb
+++ spec/sctp_spec.rb
@@ -208,7 +208,7 @@ RSpec.describe SCTP::Socket do
       end
     end
 
-    context "get_status" do
+    context "get_retransmission_info" do
       before do
         @server.bindx(:addresses => addresses, :port => port, :reuse_addr => true)
         @server.listen
@@ -229,6 +229,28 @@ RSpec.describe SCTP::Socket do
 
       example "get_rto_info is an alias for get_retransmission_info" do
         expect(@server.method(:get_rto_info)).to eq(@server.method(:get_retransmission_info))
+      end
+    end
+
+    context "get_association_info" do
+      before do
+        @server.bindx(:addresses => addresses, :port => port, :reuse_addr => true)
+        @server.listen
+        @socket.connectx(:addresses => addresses, :port => port, :reuse_addr => true)
+      end
+
+      example "get_association_info returns expected value" do
+        info = @server.get_association_info
+        expect(info.association_id).to be_a(Integer)
+        expect(info.max_retransmission_count).to be_a(Integer)
+        expect(info.number_peer_destinations).to be_a(Integer)
+        expect(info.peer_receive_window).to be_a(Integer)
+        expect(info.local_receive_window).to be_a(Integer)
+        expect(info.cookie_life).to be_a(Integer)
+      end
+
+      example "get_association_info does not accept any arguments" do
+        expect{ @server.get_association_info(true) }.to raise_error(ArgumentError)
       end
     end
   end
