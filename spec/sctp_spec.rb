27d1ad6505fe757590ab4a67ebb25ec39fe5a533
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-02-07 06:31:16 -0500

    Add specs for constants.

--- spec/sctp_spec.rb
+++ spec/sctp_spec.rb
@@ -3,6 +3,8 @@
 #
 # These specs assume you've created two dummy interfaces at 1.1.1.1 and
 # 1.1.1.2. Without these the specs will fail.
+#
+# Run the `rake create_dummy_links` task first to do this for you if needed.
 ##############################################################################
 require 'socket'
 require 'sctp/socket'
@@ -252,6 +254,72 @@ RSpec.describe SCTP::Socket do
       example "get_association_info does not accept any arguments" do
         expect{ @server.get_association_info(true) }.to raise_error(ArgumentError)
       end
+    end
+  end
+
+  context "constants" do
+    example "SCTP_CLOSED" do
+      expect(described_class::SCTP_CLOSED).to be_a(Integer)
+    end
+
+    example "SCTP_COOKIE_WAIT" do
+      expect(described_class::SCTP_COOKIE_WAIT).to be_a(Integer)
+    end
+
+    example "SCTP_COOKIE_ECHOED" do
+      expect(described_class::SCTP_COOKIE_ECHOED).to be_a(Integer)
+    end
+
+    example "SCTP_ESTABLISHED" do
+      expect(described_class::SCTP_ESTABLISHED).to be_a(Integer)
+    end
+
+    example "SCTP_SHUTDOWN_PENDING" do
+      expect(described_class::SCTP_SHUTDOWN_PENDING).to be_a(Integer)
+    end
+
+    example "SCTP_SHUTDOWN_SENT" do
+      expect(described_class::SCTP_SHUTDOWN_SENT).to be_a(Integer)
+    end
+
+    example "SCTP_SHUTDOWN_RECEIVED" do
+      expect(described_class::SCTP_SHUTDOWN_RECEIVED).to be_a(Integer)
+    end
+
+    example "SCTP_SHUTDOWN_ACK_SENT" do
+      expect(described_class::SCTP_SHUTDOWN_ACK_SENT).to be_a(Integer)
+    end
+
+    example "SCTP_BINDX_ADD_ADDR" do
+      expect(described_class::SCTP_BINDX_ADD_ADDR).to be_a(Integer)
+    end
+
+    example "SCTP_BINDX_REM_ADDR" do
+      expect(described_class::SCTP_BINDX_REM_ADDR).to be_a(Integer)
+    end
+
+    example "SCTP_UNORDERED" do
+      expect(described_class::SCTP_UNORDERED).to be_a(Integer)
+    end
+
+    example "SCTP_ADDR_OVER" do
+      expect(described_class::SCTP_ADDR_OVER).to be_a(Integer)
+    end
+
+    example "SCTP_ABORT" do
+      expect(described_class::SCTP_ABORT).to be_a(Integer)
+    end
+
+    example "SCTP_EOF" do
+      expect(described_class::SCTP_EOF).to be_a(Integer)
+    end
+
+    example "SCTP_SENDALL" do
+      expect(described_class::SCTP_SENDALL).to be_a(Integer)
+    end
+
+    example "MSG_NOTIFICATION" do
+      expect(described_class::MSG_NOTIFICATION).to be_a(Integer)
     end
   end
 
