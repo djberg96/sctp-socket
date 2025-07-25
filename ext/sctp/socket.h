ef599f19f29a6dba4d0323edd88dde87da5023fd
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-07-24 21:56:34 -0400

    Remove unused client and server code.

--- ext/sctp/socket.h
+++ ext/sctp/socket.h
@@ -7,12 +7,8 @@
 VALUE rb_hash_aref2(VALUE, const char*);
 
 void Init_socket();
-void Init_server();
-void Init_client();
 
 extern VALUE mSCTP;
 extern VALUE cSocket;
-extern VALUE cClient;
-extern VALUE cServer;
 
 #endif
--- ext/sctp/socket/client.c
+++
@@ -1,7 +0,0 @@
-#include <sctp/socket.h>
-
-VALUE cClient;
-
-void Init_client(){
-  cClient = rb_define_class_under(cSocket, "Client", rb_cObject);
-}
