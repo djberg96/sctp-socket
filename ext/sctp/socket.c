9c7b30c41aaa8463ca9f516020dd8f7d7e695ee0
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-07-28 23:55:45 -0400

    Ignore bundle files.

--- ext/sctp/socket.c
+++ ext/sctp/socket.c
@@ -118,6 +118,64 @@ static VALUE sctp_socket_close(VALUE self) {
     return Qtrue;
 }
 
+static VALUE sctp_socket_setsockopt(VALUE self, VALUE level, VALUE optname, VALUE optval) {
+    sctp_socket_wrapper *wrapper;
+    Data_Get_Struct(self, sctp_socket_wrapper, wrapper);
+    int c_level = NUM2INT(level);
+    int c_optname = NUM2INT(optname);
+    Check_Type(optval, T_STRING);
+    const char *buf = StringValueCStr(optval);
+    socklen_t optlen = RSTRING_LEN(optval);
+    if (usrsctp_setsockopt(wrapper->sock, c_level, c_optname, buf, optlen) < 0)
+        rb_sys_fail("usrsctp_setsockopt");
+    return Qtrue;
+}
+
+static VALUE sctp_socket_getsockopt(VALUE self, VALUE level, VALUE optname, VALUE optlen) {
+    sctp_socket_wrapper *wrapper;
+    Data_Get_Struct(self, sctp_socket_wrapper, wrapper);
+    int c_level = NUM2INT(level);
+    int c_optname = NUM2INT(optname);
+    socklen_t c_optlen = NUM2INT(optlen);
+    char *buf = ALLOC_N(char, c_optlen);
+    socklen_t len = c_optlen;
+    if (usrsctp_getsockopt(wrapper->sock, c_level, c_optname, buf, &len) < 0) {
+        xfree(buf);
+        rb_sys_fail("usrsctp_getsockopt");
+    }
+    VALUE str = rb_str_new(buf, len);
+    xfree(buf);
+    return str;
+}
+
+static VALUE sctp_socket_shutdown(VALUE self, VALUE how) {
+    sctp_socket_wrapper *wrapper;
+    Data_Get_Struct(self, sctp_socket_wrapper, wrapper);
+    int c_how = NUM2INT(how);
+    if (usrsctp_shutdown(wrapper->sock, c_how) < 0)
+        rb_sys_fail("usrsctp_shutdown");
+    return Qtrue;
+}
+
+static VALUE sctp_socket_status(VALUE self) {
+    sctp_socket_wrapper *wrapper;
+    Data_Get_Struct(self, sctp_socket_wrapper, wrapper);
+    struct sctp_status status;
+    socklen_t len = sizeof(status);
+    if (usrsctp_getsockopt(wrapper->sock, IPPROTO_SCTP, SCTP_STATUS, &status, &len) < 0)
+        rb_sys_fail("usrsctp_getsockopt (SCTP_STATUS)");
+    VALUE hash = rb_hash_new();
+    rb_hash_aset(hash, ID2SYM(rb_intern("assoc_id")), INT2NUM(status.sstat_assoc_id));
+    rb_hash_aset(hash, ID2SYM(rb_intern("state")), INT2NUM(status.sstat_state));
+    rb_hash_aset(hash, ID2SYM(rb_intern("instrms")), INT2NUM(status.sstat_instrms));
+    rb_hash_aset(hash, ID2SYM(rb_intern("outstrms")), INT2NUM(status.sstat_outstrms));
+    rb_hash_aset(hash, ID2SYM(rb_intern("rwnd")), INT2NUM(status.sstat_rwnd));
+    rb_hash_aset(hash, ID2SYM(rb_intern("unackdata")), INT2NUM(status.sstat_unackdata));
+    rb_hash_aset(hash, ID2SYM(rb_intern("penddata")), INT2NUM(status.sstat_penddata));
+    rb_hash_aset(hash, ID2SYM(rb_intern("fragmentation_point")), INT2NUM(status.sstat_fragmentation_point));
+    return hash;
+}
+
 void Init_socket(void) {
     VALUE mSCTP = rb_define_module("SCTP");
     cSCTPSocket = rb_define_class_under(mSCTP, "Socket", rb_cObject);
@@ -128,4 +186,8 @@ void Init_socket(void) {
     rb_define_method(cSCTPSocket, "send", sctp_socket_send, 1);
     rb_define_method(cSCTPSocket, "recv", sctp_socket_recv, 1);
     rb_define_method(cSCTPSocket, "close", sctp_socket_close, 0);
+    rb_define_method(cSCTPSocket, "setsockopt", sctp_socket_setsockopt, 3);
+    rb_define_method(cSCTPSocket, "getsockopt", sctp_socket_getsockopt, 3);
+    rb_define_method(cSCTPSocket, "shutdown", sctp_socket_shutdown, 1);
+    rb_define_method(cSCTPSocket, "status", sctp_socket_status, 0);
 }
