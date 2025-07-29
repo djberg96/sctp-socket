ad32c191ebf8b96928b195ed5fb5fc25f60aaf72
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-07-29 05:13:47 -0400

    Fix dumb AI mistakes, still not working though.

--- ext/sctp/socket.c
+++ ext/sctp/socket.c
@@ -5,9 +5,116 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 
+typedef struct {
+    struct socket *sock;
+} sctp_socket_wrapper;
+
+#// Subscribe to SCTP events
+static VALUE sctp_socket_subscribe_events(VALUE self, VALUE event_bitmap) {
+    sctp_socket_wrapper *wrapper;
+    Data_Get_Struct(self, sctp_socket_wrapper, wrapper);
+    struct sctp_event_subscribe events;
+    memset(&events, 0, sizeof(events));
+    int bitmap = NUM2INT(event_bitmap);
+    events.sctp_data_io_event = (bitmap & 0x01) ? 1 : 0;
+    events.sctp_association_event = (bitmap & 0x02) ? 1 : 0;
+    events.sctp_address_event = (bitmap & 0x04) ? 1 : 0;
+    events.sctp_send_failure_event = (bitmap & 0x08) ? 1 : 0;
+    events.sctp_peer_error_event = (bitmap & 0x10) ? 1 : 0;
+    events.sctp_shutdown_event = (bitmap & 0x20) ? 1 : 0;
+    events.sctp_partial_delivery_event = (bitmap & 0x40) ? 1 : 0;
+    events.sctp_adaptation_layer_event = (bitmap & 0x80) ? 1 : 0;
+    events.sctp_authentication_event = (bitmap & 0x100) ? 1 : 0;
+    if (usrsctp_setsockopt(wrapper->sock, IPPROTO_SCTP, SCTP_EVENT, &events, sizeof(events)) < 0)
+        rb_sys_fail("usrsctp_setsockopt (SCTP_EVENT)");
+    return Qtrue;
+}
+
+// Set SCTP_INITMSG options
+static VALUE sctp_socket_set_initmsg(VALUE self, VALUE in_streams, VALUE out_streams) {
+    sctp_socket_wrapper *wrapper;
+    Data_Get_Struct(self, sctp_socket_wrapper, wrapper);
+    struct sctp_initmsg initmsg;
+    memset(&initmsg, 0, sizeof(initmsg));
+    initmsg.sinit_num_ostreams = NUM2INT(out_streams);
+    initmsg.sinit_max_instreams = NUM2INT(in_streams);
+    if (usrsctp_setsockopt(wrapper->sock, IPPROTO_SCTP, SCTP_INITMSG, &initmsg, sizeof(initmsg)) < 0)
+        rb_sys_fail("usrsctp_setsockopt (SCTP_INITMSG)");
+    return Qtrue;
+}
+
+// Advanced sendv (with stream, ppid, flags, context, assoc_id)
+static VALUE sctp_socket_sendv(int argc, VALUE *argv, VALUE self) {
+    sctp_socket_wrapper *wrapper;
+    Data_Get_Struct(self, sctp_socket_wrapper, wrapper);
+    VALUE data, stream, ppid, flags, context, assoc_id;
+    rb_scan_args(argc, argv, "15", &data, &stream, &ppid, &flags, &context, &assoc_id);
+    Check_Type(data, T_STRING);
+    const char *buf = StringValueCStr(data);
+    size_t len = RSTRING_LEN(data);
+    struct sctp_sndinfo sndinfo;
+    memset(&sndinfo, 0, sizeof(sndinfo));
+    sndinfo.snd_sid = stream != Qnil ? NUM2INT(stream) : 0;
+    sndinfo.snd_flags = flags != Qnil ? NUM2INT(flags) : 0;
+    sndinfo.snd_ppid = ppid != Qnil ? NUM2UINT(ppid) : 0;
+    sndinfo.snd_context = context != Qnil ? NUM2UINT(context) : 0;
+    sndinfo.snd_assoc_id = assoc_id != Qnil ? NUM2UINT(assoc_id) : 0;
+    ssize_t sent = usrsctp_sendv(wrapper->sock, buf, len, NULL, 0, &sndinfo, sizeof(sndinfo), SCTP_SENDV_SNDINFO, 0);
+    if (sent < 0) rb_sys_fail("usrsctp_sendv (SNDINFO)");
+    return INT2NUM(sent);
+}
+
+// Advanced recvv (with stream, ppid, flags, context, assoc_id)
+static VALUE sctp_socket_recvv(VALUE self, VALUE maxlen) {
+    sctp_socket_wrapper *wrapper;
+    Data_Get_Struct(self, sctp_socket_wrapper, wrapper);
+    long len = NUM2LONG(maxlen);
+    char *buf = ALLOC_N(char, len);
+    struct sctp_rcvinfo rcvinfo;
+    socklen_t info_len = sizeof(rcvinfo);
+    ssize_t recvd = usrsctp_recvv(wrapper->sock, buf, len, NULL, NULL, &rcvinfo, &info_len, SCTP_RECVV_RCVINFO, 0);
+    if (recvd < 0) {
+        xfree(buf);
+        rb_sys_fail("usrsctp_recvv (RCVINFO)");
+    }
+    VALUE hash = rb_hash_new();
+    rb_hash_aset(hash, ID2SYM(rb_intern("data")), rb_str_new(buf, recvd));
+    rb_hash_aset(hash, ID2SYM(rb_intern("stream")), INT2NUM(rcvinfo.rcv_sid));
+    rb_hash_aset(hash, ID2SYM(rb_intern("flags")), INT2NUM(rcvinfo.rcv_flags));
+    rb_hash_aset(hash, ID2SYM(rb_intern("ppid")), UINT2NUM(rcvinfo.rcv_ppid));
+    rb_hash_aset(hash, ID2SYM(rb_intern("context")), UINT2NUM(rcvinfo.rcv_context));
+    rb_hash_aset(hash, ID2SYM(rb_intern("assoc_id")), UINT2NUM(rcvinfo.rcv_assoc_id));
+    xfree(buf);
+    return hash;
+}
+
+static VALUE sctp_socket_recvv(VALUE self, VALUE maxlen) {
+    sctp_socket_wrapper *wrapper;
+    Data_Get_Struct(self, sctp_socket_wrapper, wrapper);
+    long len = NUM2LONG(maxlen);
+    char *buf = ALLOC_N(char, len);
+    struct sctp_rcvinfo rcvinfo;
+    socklen_t info_len = sizeof(rcvinfo);
+    ssize_t recvd = usrsctp_recvv(wrapper->sock, buf, len, NULL, NULL, &rcvinfo, &info_len, SCTP_RECVV_RCVINFO, 0);
+    if (recvd < 0) {
+        xfree(buf);
+        rb_sys_fail("usrsctp_recvv (RCVINFO)");
+    }
+    VALUE hash = rb_hash_new();
+    rb_hash_aset(hash, ID2SYM(rb_intern("data")), rb_str_new(buf, recvd));
+    rb_hash_aset(hash, ID2SYM(rb_intern("stream")), INT2NUM(rcvinfo.rcv_sid));
+    rb_hash_aset(hash, ID2SYM(rb_intern("flags")), INT2NUM(rcvinfo.rcv_flags));
+    rb_hash_aset(hash, ID2SYM(rb_intern("ppid")), UINT2NUM(rcvinfo.rcv_ppid));
+    rb_hash_aset(hash, ID2SYM(rb_intern("context")), UINT2NUM(rcvinfo.rcv_context));
+    rb_hash_aset(hash, ID2SYM(rb_intern("assoc_id")), UINT2NUM(rcvinfo.rcv_assoc_id));
+    xfree(buf);
+    return hash;
+}
+
 static VALUE cSCTPSocket;
 
 typedef struct {
+// ...existing code...
     struct socket *sock;
 } sctp_socket_wrapper;
 
@@ -158,6 +265,84 @@ static VALUE sctp_socket_shutdown(VALUE self, VALUE how) {
 }
 
 static VALUE sctp_socket_status(VALUE self) {
+// Subscribe to SCTP events
+static VALUE sctp_socket_subscribe_events(VALUE self, VALUE event_bitmap) {
+    sctp_socket_wrapper *wrapper;
+    Data_Get_Struct(self, sctp_socket_wrapper, wrapper);
+    struct sctp_event_subscribe events;
+    memset(&events, 0, sizeof(events));
+    int bitmap = NUM2INT(event_bitmap);
+    events.sctp_data_io_event = (bitmap & 0x01) ? 1 : 0;
+    events.sctp_association_event = (bitmap & 0x02) ? 1 : 0;
+    events.sctp_address_event = (bitmap & 0x04) ? 1 : 0;
+    events.sctp_send_failure_event = (bitmap & 0x08) ? 1 : 0;
+    events.sctp_peer_error_event = (bitmap & 0x10) ? 1 : 0;
+    events.sctp_shutdown_event = (bitmap & 0x20) ? 1 : 0;
+    events.sctp_partial_delivery_event = (bitmap & 0x40) ? 1 : 0;
+    events.sctp_adaptation_layer_event = (bitmap & 0x80) ? 1 : 0;
+    events.sctp_authentication_event = (bitmap & 0x100) ? 1 : 0;
+    if (usrsctp_setsockopt(wrapper->sock, IPPROTO_SCTP, SCTP_EVENT, &events, sizeof(events)) < 0)
+        rb_sys_fail("usrsctp_setsockopt (SCTP_EVENT)");
+    return Qtrue;
+}
+
+// Set SCTP_INITMSG options
+static VALUE sctp_socket_set_initmsg(VALUE self, VALUE in_streams, VALUE out_streams) {
+    sctp_socket_wrapper *wrapper;
+    Data_Get_Struct(self, sctp_socket_wrapper, wrapper);
+    struct sctp_initmsg initmsg;
+    memset(&initmsg, 0, sizeof(initmsg));
+    initmsg.sinit_num_ostreams = NUM2INT(out_streams);
+    initmsg.sinit_max_instreams = NUM2INT(in_streams);
+    if (usrsctp_setsockopt(wrapper->sock, IPPROTO_SCTP, SCTP_INITMSG, &initmsg, sizeof(initmsg)) < 0)
+        rb_sys_fail("usrsctp_setsockopt (SCTP_INITMSG)");
+    return Qtrue;
+}
+
+// Advanced sendv (with stream, ppid, flags, context, assoc_id)
+static VALUE sctp_socket_sendv(int argc, VALUE *argv, VALUE self) {
+    sctp_socket_wrapper *wrapper;
+    Data_Get_Struct(self, sctp_socket_wrapper, wrapper);
+    VALUE data, stream, ppid, flags, context, assoc_id;
+    rb_scan_args(argc, argv, "15", &data, &stream, &ppid, &flags, &context, &assoc_id);
+    Check_Type(data, T_STRING);
+    const char *buf = StringValueCStr(data);
+    size_t len = RSTRING_LEN(data);
+    struct sctp_sndinfo sndinfo;
+    memset(&sndinfo, 0, sizeof(sndinfo));
+    sndinfo.snd_sid = stream != Qnil ? NUM2INT(stream) : 0;
+    sndinfo.snd_flags = flags != Qnil ? NUM2INT(flags) : 0;
+    sndinfo.snd_ppid = ppid != Qnil ? NUM2UINT(ppid) : 0;
+    sndinfo.snd_context = context != Qnil ? NUM2UINT(context) : 0;
+    sndinfo.snd_assoc_id = assoc_id != Qnil ? NUM2UINT(assoc_id) : 0;
+    ssize_t sent = usrsctp_sendv(wrapper->sock, buf, len, NULL, 0, &sndinfo, sizeof(sndinfo), SCTP_SENDV_SNDINFO, 0);
+    if (sent < 0) rb_sys_fail("usrsctp_sendv (SNDINFO)");
+    return INT2NUM(sent);
+}
+
+// Advanced recvv (with stream, ppid, flags, context, assoc_id)
+static VALUE sctp_socket_recvv(VALUE self, VALUE maxlen) {
+    sctp_socket_wrapper *wrapper;
+    Data_Get_Struct(self, sctp_socket_wrapper, wrapper);
+    long len = NUM2LONG(maxlen);
+    char *buf = ALLOC_N(char, len);
+    struct sctp_rcvinfo rcvinfo;
+    socklen_t info_len = sizeof(rcvinfo);
+    ssize_t recvd = usrsctp_recvv(wrapper->sock, buf, len, NULL, NULL, &rcvinfo, &info_len, SCTP_RECVV_RCVINFO, 0);
+    if (recvd < 0) {
+        xfree(buf);
+        rb_sys_fail("usrsctp_recvv (RCVINFO)");
+    }
+    VALUE hash = rb_hash_new();
+    rb_hash_aset(hash, ID2SYM(rb_intern("data")), rb_str_new(buf, recvd));
+    rb_hash_aset(hash, ID2SYM(rb_intern("stream")), INT2NUM(rcvinfo.rcv_sid));
+    rb_hash_aset(hash, ID2SYM(rb_intern("flags")), INT2NUM(rcvinfo.rcv_flags));
+    rb_hash_aset(hash, ID2SYM(rb_intern("ppid")), UINT2NUM(rcvinfo.rcv_ppid));
+    rb_hash_aset(hash, ID2SYM(rb_intern("context")), UINT2NUM(rcvinfo.rcv_context));
+    rb_hash_aset(hash, ID2SYM(rb_intern("assoc_id")), UINT2NUM(rcvinfo.rcv_assoc_id));
+    xfree(buf);
+    return hash;
+}
     sctp_socket_wrapper *wrapper;
     Data_Get_Struct(self, sctp_socket_wrapper, wrapper);
     struct sctp_status status;
@@ -176,6 +361,7 @@ static VALUE sctp_socket_status(VALUE self) {
     return hash;
 }
 
+
 void Init_socket(void) {
     VALUE mSCTP = rb_define_module("SCTP");
     cSCTPSocket = rb_define_class_under(mSCTP, "Socket", rb_cObject);
@@ -190,4 +376,8 @@ void Init_socket(void) {
     rb_define_method(cSCTPSocket, "getsockopt", sctp_socket_getsockopt, 3);
     rb_define_method(cSCTPSocket, "shutdown", sctp_socket_shutdown, 1);
     rb_define_method(cSCTPSocket, "status", sctp_socket_status, 0);
+    rb_define_method(cSCTPSocket, "subscribe_events", sctp_socket_subscribe_events, 1);
+    rb_define_method(cSCTPSocket, "set_initmsg", sctp_socket_set_initmsg, 2);
+    rb_define_method(cSCTPSocket, "sendv", sctp_socket_sendv, -1);
+    rb_define_method(cSCTPSocket, "recvv", sctp_socket_recvv, 1);
 }
