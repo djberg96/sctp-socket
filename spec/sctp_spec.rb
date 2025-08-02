7f58a6c4741bfd6a34a81a949d2fcad93c34ca0c
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-08-02 08:30:08 -0400

    Add get_default_send_params specs.

--- spec/sctp_spec.rb
+++ spec/sctp_spec.rb
@@ -453,6 +453,99 @@ RSpec.describe SCTP::Socket do
         expect(state1).to eq(state2)
       end
     end
+
+    context "get_default_send_params" do
+      before do
+        @server.bindx(:addresses => addresses, :port => port, :reuse_addr => true)
+        @server.listen
+        @socket.connectx(:addresses => addresses, :port => port, :reuse_addr => true)
+      end
+
+      example "get_default_send_params basic functionality" do
+        expect(@socket).to respond_to(:get_default_send_params)
+      end
+
+      example "get_default_send_params returns expected struct type" do
+        params = @socket.get_default_send_params
+        expect(params).to be_a(Struct)
+        expect(params.class.name).to match(/DefaultSendParams/)
+      end
+
+      example "get_default_send_params returns struct with expected members" do
+        params = @socket.get_default_send_params
+        expect(params).to respond_to(:stream)
+        expect(params).to respond_to(:ssn)
+        expect(params).to respond_to(:flags)
+        expect(params).to respond_to(:ppid)
+        expect(params).to respond_to(:context)
+        expect(params).to respond_to(:ttl)
+        expect(params).to respond_to(:tsn)
+        expect(params).to respond_to(:cumtsn)
+        expect(params).to respond_to(:association_id)
+      end
+
+      example "get_default_send_params returns struct with expected value types" do
+        params = @socket.get_default_send_params
+        expect(params.stream).to be_a(Integer)
+        expect(params.ssn).to be_a(Integer)
+        expect(params.flags).to be_a(Integer)
+        expect(params.ppid).to be_a(Integer)
+        expect(params.context).to be_a(Integer)
+        expect(params.ttl).to be_a(Integer)
+        expect(params.tsn).to be_a(Integer)
+        expect(params.cumtsn).to be_a(Integer)
+        expect(params.association_id).to be_a(Integer)
+      end
+
+      example "get_default_send_params returns reasonable default values" do
+        params = @socket.get_default_send_params
+        expect(params.stream).to be >= 0
+        expect(params.ssn).to be >= 0
+        expect(params.flags).to be >= 0
+        expect(params.ppid).to be >= 0
+        expect(params.context).to be >= 0
+        expect(params.ttl).to be >= 0
+        expect(params.tsn).to be >= 0
+        expect(params.cumtsn).to be >= 0
+        expect(params.association_id).to be >= 0
+      end
+
+      xexample "get_default_send_params association_id matches socket's association_id" do
+        params = @socket.get_default_send_params
+        expect(params.association_id).to eq(@socket.association_id)
+      end
+
+      example "get_default_send_params does not accept any arguments" do
+        expect{ @socket.get_default_send_params(true) }.to raise_error(ArgumentError)
+        expect{ @socket.get_default_send_params({}) }.to raise_error(ArgumentError)
+        expect{ @socket.get_default_send_params(1, 2) }.to raise_error(ArgumentError)
+      end
+
+      example "get_default_send_params returns consistent values on multiple calls" do
+        params1 = @socket.get_default_send_params
+        params2 = @socket.get_default_send_params
+        expect(params1.stream).to eq(params2.stream)
+        expect(params1.ssn).to eq(params2.ssn)
+        expect(params1.flags).to eq(params2.flags)
+        expect(params1.ppid).to eq(params2.ppid)
+        expect(params1.context).to eq(params2.context)
+        expect(params1.ttl).to eq(params2.ttl)
+        expect(params1.association_id).to eq(params2.association_id)
+      end
+
+      example "get_default_send_params stream is within valid range" do
+        params = @socket.get_default_send_params
+        # Stream IDs should be reasonable (typically < 65536)
+        expect(params.stream).to be < 65536
+      end
+
+      example "get_default_send_params flags represent valid SCTP flags" do
+        params = @socket.get_default_send_params
+        # Flags should be a valid bitmask (non-negative integer)
+        expect(params.flags).to be >= 0
+        expect(params.flags).to be < (1 << 32) # Should fit in 32-bit unsigned
+      end
+    end
   end
 
   context "constants" do
