886ea2f2646b28b6a45bb1a32fb6ff061a5c458c
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-08-05 06:12:17 -0400

    Fix version comment.

--- ext/sctp/socket.c
+++ ext/sctp/socket.c
@@ -2899,7 +2899,7 @@ void Init_socket(void){
   rb_define_attr(cSocket, "association_id", 1, 1);
   rb_define_attr(cSocket, "port", 1, 1);
 
-  /* 0.1.4: The version of this library */
+  /* 0.2.0: The version of this library */
   rb_define_const(cSocket, "VERSION", rb_str_new2("0.2.0"));
 
   /* send flags */
