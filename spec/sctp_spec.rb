7cc512eb77c7dae4989f64348ee2135eaab70e04
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-08-07 01:59:22 -0400

    Remove connectx spec, moved to its own file.

--- spec/sctp_spec.rb
+++ spec/sctp_spec.rb
@@ -105,28 +105,6 @@ RSpec.describe SCTP::Socket do
       end
     end
 
-    context "connectx" do
-      before do
-        @server.bindx(:port => port, :reuse_addr => true)
-        @server.listen
-      end
-
-      example "connectx basic check" do
-        expect{ @socket.connectx(:addresses => addresses, :port => port) }.not_to raise_error
-      end
-
-      example "association ID is set after connectx" do
-        @socket.connectx(:addresses => addresses, :port => port)
-        expect(@socket.association_id).to be > 0
-      end
-
-      example "connectx requires both a port and an array of addresses" do
-        expect{ @socket.connectx }.to raise_error(ArgumentError)
-        expect{ @socket.connectx(:port => port) }.to raise_error(ArgumentError)
-        expect{ @socket.connectx(:addresses => addresses) }.to raise_error(ArgumentError)
-      end
-    end
-
     context "getpeernames" do
       before do
         @server.bindx(:addresses => addresses, :port => port, :reuse_addr => true)
